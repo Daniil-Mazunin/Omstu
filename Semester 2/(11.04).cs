be956b1c30e5520b7af118259e4059dc7ca28445
Daniil-Mazunin <dsss6872@gmail.com>
2025-04-20 12:38:29 +0600

    Add files via upload

---
+++ Semester 2/(11.04).cs
@@ -0,0 +1,47 @@
+/* Ќеобходимо реализовать обобщенный класс, который позвол€ет хранить в массиве объекты любого типа
+в классе определить дл€ добавлени€ данных в массив, удалени€ из массива по индексу и 
+получени€ из массива по индексу */
+using System;
+class Array<T>
+{
+    private T[] array = new T[0];
+    public void Add(T element)
+    {
+        T[] new_array = new T[array.Length + 1];
+        for (int i = 0; i < array.Length; i++)
+        {
+            new_array[i] = array[i];
+        }
+        new_array[array.Length] = element;
+        array = new_array;
+    }
+    public void Delete(int index)
+    {
+        T[] new_array = new T[array.Length - 1];
+        int j = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i != index)
+            {
+                new_array[j++] = array[i];
+            }
+        }
+        array = new_array;
+    }
+    public T Get(int index)
+    {
+        return array[index];
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        var array = new Array<int>();
+        array.Add(1);
+        array.Add(2);
+        Console.WriteLine(array.Get(0));
+        array.Delete(1);
+        Console.WriteLine(array.Get(0));
+    }
+}
